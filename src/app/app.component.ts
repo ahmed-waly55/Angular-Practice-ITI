1c12bd663010e8b6ba8a9a1ab645eb24874c0e9a
ahmed waly <ahmedwaly074@gmail.com>
2025-01-19 22:07:23 +0200

    create template include TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template:'<h1>Hello inclide {{title}}</h1>',
   standalone: false,
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
+  styles:[`h1{color:green;}`]
 })
 export class AppComponent {
   title = 'angular';
 }
+ 
