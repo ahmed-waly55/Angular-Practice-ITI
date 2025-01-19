5db36b59275d3179b3594a400ac4d9c9b5d826e1
ahmed waly <ahmedwaly074@gmail.com>
2025-01-19 22:45:08 +0200

    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template:'<h1>Hello inclide {{title}}</h1>',
+  templateUrl: './app.component.html',
+  // template:'<h1>Hello inclide {{title}}</h1>',
   standalone: false,
-  // styleUrl: './app.component.css'
-  styles:[`h1{color:green;}`]
+  styleUrl: './app.component.css'
+  // styles:[`h1{color:green;}`]
 })
 export class AppComponent {
   title = 'angular';
