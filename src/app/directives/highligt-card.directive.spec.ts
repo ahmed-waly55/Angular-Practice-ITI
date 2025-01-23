3aa9c99cad3e68a28378342d38fe2150dc92b01f
ahmed waly <ahmedwaly074@gmail.com>
2025-01-23 17:24:52 +0200

    learn Input , HostListener Diractive and practice

---
+++ src/app/directives/highligt-card.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighligtCardDirective } from './highligt-card.directive';
+
+describe('HighligtCardDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighligtCardDirective();
+    expect(directive).toBeTruthy();
+  });
+});
