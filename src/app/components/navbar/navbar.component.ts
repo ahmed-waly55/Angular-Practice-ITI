61be956e91b42d71685403d41d406046bc01d475
ahmed waly <ahmedwaly074@gmail.com>
2025-01-29 17:39:38 +0200

    edit navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { StaticProductsService } from '../../services/static-products.service';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
+  providers:[StaticProductsService],
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(private _StaticProductsService:StaticProductsService){
+    
+  }
 
 }
